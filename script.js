d1fa9cfa9bad24c1fc8931f99dac1cfaf8470f9d
Ashmin <ashmin2802@gmail.com>
2024-01-14 14:52:14 +0530

    Done

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function toggleMenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle('open');
-    icon.classList.toggle('open')
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
+
+function startCircularProgress(circularProgressClass, progressValueClass, progressEndValue) {
+    let circularProgress = document.querySelector(`.${circularProgressClass}`);
+    let progressValue = document.querySelector(`.${progressValueClass}`);
+
+    let progressStartValue = 0;
+    let speed = 30;
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}%`;
+        circularProgress.style.background = `conic-gradient(#000 ${progressStartValue * 3.6}deg, #ededed 0deg)`;
+
+        if (progressStartValue === progressEndValue) {
+            clearInterval(progress);
+        }
+        console.log(progressStartValue);
+    }, speed);
+}
+
+// Start four progress bars with different end values
+startCircularProgress("circular-progress1", "progress-value1", 99);
+startCircularProgress("circular-progress2", "progress-value2", 100);
+startCircularProgress("circular-progress3", "progress-value3", 50);
+startCircularProgress("circular-progress4", "progress-value4", 55);
+startCircularProgress("circular-progress5", "progress-value5", 10);
+startCircularProgress("circular-progress6", "progress-value6", 30);
+// startCircularProgress("circular-progress7", "progress-value7", 100);
+// startCircularProgress("circular-progress8", "progress-value8", 100);
