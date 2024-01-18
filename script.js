df8aeaba27cf1c2fdd2819319b522c21a26c8a9f
Ashmin <ashmin2802@gmail.com>
2024-01-18 16:23:47 +0530

    Done

--- script.js
+++ script.js
@@ -5,19 +5,42 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-ScrollReveal({ 
-    // reset: true,
-    distance:'80px',
-    duration:2200,
-    delay:200
+// ----------------------------------scroll indicator-----------------------------------------------------
+
+window.addEventListener("scroll", () => {
+    const indicatorBar = document.querySelector(".scroll-indicator-bar");
+
+    const pageScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const hight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollValue = (pageScroll / hight) * 100;
+
+    indicatorBar.style.width = scrollValue + "%";
 });
-ScrollReveal().reveal('#desktop-nav, #hamburger-nav .logo', { delay: 500 });
-ScrollReveal().reveal('#profile, .section__text__p1-2, .prject-btn button, .details-container p, .text-container p, .frendend_backend h3, .contact-info-container p,a', { delay: 200, origin: 'right'});
-ScrollReveal().reveal('.title, .section__pic__container img, .project-title , .details-container h3 ', { delay: 500, origin: 'left'});
-ScrollReveal().reveal('.section__pic-container ,#about,  #experience, #projects, #contact, footer  ', { delay: 600, origin: 'bottom'});
-ScrollReveal().reveal('#socials-container img, .details-container img, .contact-info-container img, .nav-links li, .menu-links li,a , .progress-container ', { delay: 600, origin: 'top', interval: 100});
 
-
+// -------------------------- skills circle percentage------------------------------------------------------------
+ScrollReveal({
+    // reset: true,
+    distance: "80px",
+    duration: 2200,
+    delay: 200,
+});
+ScrollReveal().reveal("#desktop-nav, #hamburger-nav .logo", { delay: 500 });
+ScrollReveal().reveal(
+    "#profile, .section__text__p1-2, .prject-btn button, .details-container p, .text-container p, .frendend_backend h3, .contact-info-container p,a",
+    { delay: 200, origin: "right" }
+);
+ScrollReveal().reveal(".title, .section__pic__container img, .project-title , .details-container h3 ", {
+    delay: 500,
+    origin: "left",
+});
+ScrollReveal().reveal(".section__pic-container ,#about,  #experience, #projects, #contact, footer  ", {
+    delay: 600,
+    origin: "bottom",
+});
+ScrollReveal().reveal(
+    "#socials-container img, .details-container img, .contact-info-container img, .nav-links li, .menu-links li,a , .progress-container ",
+    { delay: 600, origin: "top", interval: 100 }
+);
 
 function startCircularProgress(circularProgressClass, progressValueClass, progressEndValue) {
     let circularProgress = document.querySelector(`.${circularProgressClass}`);
@@ -50,3 +73,5 @@ startCircularProgress("circular-progress7", "progress-value7", 20);
 startCircularProgress("circular-progress8", "progress-value8", 50);
 // startCircularProgress("circular-progress9", "progress-value9", 1);
 // startCircularProgress("circular-progress10", "progress-value10", 1);
+
+// -------------------------------------------------------------------
